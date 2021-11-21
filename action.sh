ecc9685ca2af86caa803a4560ab0e1917d5f4278
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-11-21 21:32:31 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=main
-git clone -b $branch https://github.com/satyanandatripathi/Ultroid /root/TeamUltroid
+git clone https://github.com/satyanandatripathi/video-stream /root/TeamUltroid
 cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
-docker run --privileged --env-file .env --rm -i ultroid
+docker run --privileged --env-file local.env --rm -i ultroid
