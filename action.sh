5745c21733b3f0ed385741603e71cfc127184fea
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-01-18 00:15:25 +0530

    Hurrr

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=main
 git clone https://github.com/AvikaTrivedi/YukkiMusicBot /root/TeamUltroid
-cp ultroid/local.env /root/TeamUltroid/local.env
+cp ultroid/my.env /root/TeamUltroid/my.env
 cd /root/TeamUltroid
 docker build . -t ultroid
 docker run -i ultroid
